5ef3bcd854a9854518b89a77c1dbc0fa6a1a6b75
Mario Ortega <mcor63@gmail.com>
2017-05-09 22:38:26 -0400

    Initial commit

---
+++ app/src/main/java/com/grip/ventas/epakiosco/splash_screen.java
@@ -0,0 +1,45 @@
+package com.grip.ventas.epakiosco;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.view.Window;
+
+
+public class splash_screen extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.splash_screen);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        splash_screen.this, MainActivity.class);
+                startActivity(mainIntent);
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+}
